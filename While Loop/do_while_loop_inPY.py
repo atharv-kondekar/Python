23a1fbd17118fa90745b0757d1dc55ce3ad979b8
Atharv_Kondekar <atharvkondekar96@gmail.com>
2025-03-18 05:55:28 +0530

    Add files via upload

---
+++ While Loop/do_while_loop_inPY.py
@@ -0,0 +1,7 @@
+count = 5
+
+while(count<0):
+    print(count)
+    count-=1
+else:
+    print("I am Inside the Else ")
---
+++ While Loop/do_while_loop_inPY02.py
@@ -0,0 +1,7 @@
+num = 1 
+while(num<=10): #if i make num>=10 then else executed at once 
+    print(num)
+    num+=1
+else:
+    if(num==1):
+        print(num)

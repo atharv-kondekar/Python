c8b59e429311ed540fa019304e3d7c5557bb9d86
Atharv_Kondekar <atharvkondekar96@gmail.com>
2025-03-20 10:59:18 +0530

    Add files via upload

---
+++ Numpy Lib/Creating_1D_Array01.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+list=[1,2,3,4,5,6,7]
+arr=np.array(list)
+
+print(arr)
+print(type(arr))
+print(arr.shape) #(7,) 7 col and 1 row 
---
+++ Numpy Lib/Creating_2D_Array01.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+list1=[1,2,3,4,5,6]
+list2=[6,5,4,3,2,1]
+list3=[4,5,6,1,2,3]
+
+arr=np.array([list1,list2,list3])
+
+print(arr)
+print(type(arr))
+print(arr.shape) #op= (3,6) 3 rows 6 cloumns

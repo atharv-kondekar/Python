ee84229844894b889066f768ed0a9d37a30213df
Atharv_Kondekar <atharvkondekar96@gmail.com>
2025-03-20 20:09:51 -0700

    Add files via upload

---
+++ Numpy Lib/reshape_funtion_01.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+list1=[1,2,3,4,5]
+list2=[5,4,3,2,1]
+list3=[1,2,5,4,3]
+
+arr1=np.array([list1,list2,list3])
+
+print("\n The Array without Reshape : \n",arr1)
+
+print("\n The Array After the Reshape funtion : \n",arr1.reshape(5,3))

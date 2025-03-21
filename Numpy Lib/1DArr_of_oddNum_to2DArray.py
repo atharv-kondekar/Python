70c547815fb8b64869893aa2c6a5442aad1265fb
Atharv_Kondekar <atharvkondekar96@gmail.com>
2025-03-20 20:12:55 -0700

    Add files via upload

---
+++ Numpy Lib/1DArr_of_oddNum_to2DArray.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+Arr1D=np.arange(1,20,2)
+
+print("\n THe 1D Array of ODD Numbers : \n",Arr1D)
+
+print("\n Converting Into the 2D : \n",Arr1D.reshape(5,2))
+print("\n Converting Into the 2D : \n",Arr1D.reshape(2,5))

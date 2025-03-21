be5b56d5cb68c0a7bc9940d802eea43753e48334
Atharv_Kondekar <atharvkondekar96@gmail.com>
2025-03-20 20:09:22 -0700

    Add files via upload

---
+++ Numpy Lib/arrange_funtn_mechanism_01.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+print(np.arange(1,10,1))
+print("\n",np.arange(1,20,2)) # creating 1D array 
+
+
+print("\n",np.arange(1,20,2).reshape(2,5)) # conversion of 1D to 2D 
+#2 rows 5 columns 

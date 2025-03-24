4b0c5ee8b73a6ac34ab59bfd1df9d182b81172f0
Atharv_Kondekar <atharvkondekar96@gmail.com>
2025-03-23 17:27:21 -0700

    Add files via upload

---
+++ Lists/creating_list_fromUser01.py
@@ -0,0 +1,9 @@
+name=input("\nEnter the Name : ")
+age=int(input("\nEnter the Age of the  : "))
+salary=int(input("\nEnter the salary : "))
+
+lst1=[name,age,salary]
+print("\nThe Person info : ",lst1)
+print("\nThe Person name : ",lst1[0])
+print("\nThe Person Age : ",lst1[1])
+print("\nThe person Salary : ",lst1[2])

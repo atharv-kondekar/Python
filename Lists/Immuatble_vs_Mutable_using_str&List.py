4ca24a2041f3af5d70fc7267acf45e421ca79ee5
Atharv_Kondekar <atharvkondekar96@gmail.com>
2025-03-23 17:26:27 -0700

    Add files via upload

---
+++ Lists/Immuatble_vs_Mutable_using_str&List.py
@@ -0,0 +1,27 @@
+str=input("\n Enter the String : ")
+print("\nThe Lenght of the String is : ",len(str))
+
+print(str)
+
+print("\nThe Str[0] : ",str[0])
+print("\nThe Str[4] : ",str[4])
+
+print("\nNow Changing the string : ",str," to Kondekar")
+str="Kondekar "
+
+
+print("\nThe Str[0] : ",str[0])
+print("\nThe Str[4] : ",str[4])
+
+#print("\n We Changing The letters(item in the String ) ")
+# print(str[0]='B') # It Gives you error 
+
+
+# Now List 
+
+lst=[1,2,3,4,5,6,7,8]
+print("\nThe List : ",lst)
+print("Now Chaning lst[0]=10")
+lst[0]=10
+print(lst)
+print("\n -------- Therefore The List is MUTABLE ---------- \n")
